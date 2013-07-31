aa5087f96830fa3fb9b4335baf571362f43c0282
Bogeyman <bogeyman.est@gmail.com>
2013-07-31 15:52:44 +0300

    Use Bukkit scheduler to reload synchronously.

--- src/org/superfuntime/smartreload/SmartReload.java
+++ src/org/superfuntime/smartreload/SmartReload.java
@@ -23,10 +23,51 @@ import static java.nio.file.StandardWatchEventKinds.*;
  * Time: 11:58
  */
 public class SmartReload extends JavaPlugin {
+    public static volatile boolean reload = false;
+    public Thread thread = new Thread() {
+        @SuppressWarnings("unchecked")
+        @Override
+        public void run() {
+            outer:
+            for (; ; ) {
+                WatchKey key;
+                try {
+                    key = watcher.take();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    break;
+                }
+                for (WatchEvent<?> event : key.pollEvents()) {
+                    if (event.kind() == OVERFLOW)
+                        continue;
+                    WatchEvent<Path> ev = (WatchEvent<Path>) event;
+                    Path name = ev.context();
+                    if (!name.toString().endsWith(".jar"))
+                        continue;
+                    String type = "";
+                    if (event.kind() == ENTRY_CREATE)
+                        type = "CREATED";
+                    else if (event.kind() == ENTRY_DELETE)
+                        type = "DELETED";
+                    else if (event.kind() == ENTRY_MODIFY)
+                        type = "MODIFIED";
+                    System.out.println(String.format("[SmartReload] Detected file change: %s(%s). Reloading.",
+                                                     name.toString(),
+                                                     type));
+                    SmartReload.reload = true;
+                    break outer;
+                }
+            }
+        }
+    };
     WatchService watcher;
     Path pluginsFolder = Paths.get(new File("plugins/").toURI());
 
-    @SuppressWarnings("unchecked")
+    @Override
+    public void onDisable() {
+        Bukkit.getScheduler().cancelTasks(this);
+    }
+
     @Override
     public void onEnable() {
         try {
@@ -36,39 +77,15 @@ public class SmartReload extends JavaPlugin {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        new Thread() {
+        thread.start();
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
             @Override
             public void run() {
-                for (; ; ) {
-                    WatchKey key;
-                    try {
-                        key = watcher.take();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                        break;
-                    }
-                    for (WatchEvent<?> event : key.pollEvents()) {
-                        if (event.kind() == OVERFLOW)
-                            continue;
-                        WatchEvent<Path> ev = (WatchEvent<Path>) event;
-                        Path name = ev.context();
-                        if (!name.toString().endsWith(".jar"))
-                            continue;
-                        String type = "";
-                        if (event.kind() == ENTRY_CREATE)
-                            type = "CREATED";
-                        else if (event.kind() == ENTRY_DELETE)
-                            type = "DELETED";
-                        else if (event.kind() == ENTRY_MODIFY)
-                            type = "MODIFIED";
-                        getLogger().info(String.format("Detected file change: %s(%s). Reloading.",
-                                                       name.toString(),
-                                                       type));
-                        Bukkit.dispatchCommand(Bukkit.getServer().getConsoleSender(), "reload");
-                    }
+                if (SmartReload.reload) {
+                    Bukkit.dispatchCommand(Bukkit.getServer().getConsoleSender(), "reload");
+                    SmartReload.reload = false;
                 }
             }
-        }.start();
-
+        }, 0, 20 * 3);
     }
 }
